02de8600ee03315e9ec7f6691a050b6c07ec700b
HunterBuxton <huntercbuxton@icloud.com>
2019-07-06 12:47:04 -0600

    Initial Commit

---
+++ FirestorePractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirestorePractice
+//
+//  Created by Hunter Buxton on 7/6/19.
+//  Copyright © 2019 Hunter Buxton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
